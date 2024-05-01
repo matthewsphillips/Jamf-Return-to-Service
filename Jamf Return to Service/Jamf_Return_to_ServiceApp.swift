6bd09c8a5132cfafe72d73b6a4620f730d1901cf
Matthew Phillips <matthewsphillips@me.com>
2024-05-01 12:24:31 -0700

    Initial Commit

---
+++ Jamf Return to Service/Jamf_Return_to_ServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jamf_Return_to_ServiceApp.swift
+//  Jamf Return to Service
+//
+//  Created by gandalf on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Jamf_Return_to_ServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
